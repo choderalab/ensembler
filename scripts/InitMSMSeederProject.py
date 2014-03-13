b1046b9d154440de6c8137404566ca949707ea7f
Daniel L. Parton <Danny.Parton@gmail.com>
2014-03-13 11:22:26 -0400

    Refactored scripts to use methods to contain main functionality.

--- scripts/InitMSMSeederProject.py
+++ scripts/InitMSMSeederProject.py
@@ -4,45 +4,55 @@
 #
 # Daniel L. Parton <partond@mskcc.org> - 11 Mar 2014
 
-# =========
-# Parameters
-# =========
+def init_msmseeder_project(project_toplevel_dir):
+    '''Initialize MSMSeeder project within the current directory. Creates
+    necessary subdirectories and a project metadata .yaml file.
+    '''
 
-import os, datetime, yaml, argparse
-import MSMSeeder
+    # =========
+    # Parameters
+    # =========
 
-project_dirnames = ['targets', 'templates', 'models', 'packaged-models']
-project_metadata_filepath = 'project-data.yaml'
+    import os, datetime, yaml, argparse
+    import MSMSeeder
 
-now = datetime.datetime.utcnow()
-datestamp = now.strftime(MSMSeeder.core.datestamp_format_string)
+    project_dirnames = ['targets', 'templates', 'models', 'packaged-models']
+    project_metadata_filepath = 'project-data.yaml'
 
-argparser = argparse.ArgumentParser(description='Initialize MSMSeeder project within the current directory. Creates necessary subdirectories and a project metadata .yaml file.')
-argparser.parse_args()
+    now = datetime.datetime.utcnow()
+    datestamp = now.strftime(MSMSeeder.core.datestamp_format_string)
 
-# =========
-# Create necessary project directories
-# =========
+    argparser = argparse.ArgumentParser(description='Initialize MSMSeeder project within the current directory. Creates necessary subdirectories and a project metadata .yaml file.')
+    argparser.parse_args()
 
-for dirname in project_dirnames:
-    try:
-        os.mkdir(dirname)
-        print 'Created directory "%s"' % dirname
-    except OSError as e:
-        # If directory already exists, e.errno will be set to int 17, and e.strerror will be set to int 'File exists'
-        if e.errno == 17:
-            print 'Directory "%s" already exists - will not overwrite' % dirname
-        else:
-            raise
-    
-# =========
-# Create metadata file and add datestamp
-# =========
+    # =========
+    # Create necessary project directories
+    # =========
 
-project_metadata = MSMSeeder.core.ProjectMetadata()
-init_metadata = {'init' : {'datestamp' : datestamp}}
-project_metadata.add_metadata(init_metadata)
-project_metadata.write(ofilepath=project_metadata_filepath)
+    os.chdir(project_toplevel_dir)
 
-print 'Done.'
+    for dirname in project_dirnames:
+        try:
+            os.mkdir(dirname)
+            print 'Created directory "%s"' % dirname
+        except OSError as e:
+            # If directory already exists, e.errno will be set to int 17, and e.strerror will be set to int 'File exists'
+            if e.errno == 17:
+                print 'Directory "%s" already exists - will not overwrite' % dirname
+            else:
+                raise
+        
+    # =========
+    # Create metadata file and add datestamp
+    # =========
+
+    project_metadata = MSMSeeder.core.ProjectMetadata()
+    init_metadata = {'init' : {'datestamp' : datestamp}}
+    project_metadata.add_metadata(init_metadata)
+    project_metadata.write(ofilepath=project_metadata_filepath)
+
+    print 'Done.'
+
+if __name__ == '__main__':
+    init_msmseeder_project('.')
 
