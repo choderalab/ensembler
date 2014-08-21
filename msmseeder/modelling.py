27cdb9e43aeb69e168760440472a2bf7e0a759d9
Daniel L. Parton <Danny.Parton@gmail.com>
2014-08-21 15:26:38 -0400

    Fix to build_model.py to prevent jobs hanging due to being unable to open modelling-rejected.txt file

--- msmseeder/modelling.py
+++ msmseeder/modelling.py
@@ -221,10 +221,13 @@ def build_model(target, template, verbose=False):
         return text
 
     except:
-        trbk = traceback.format_exc()
-        reject_file_path = os.path.join(models_target_dir, 'modelling-rejected.txt')
-        with open(reject_file_path, 'w') as reject_file:
-            reject_file.write(trbk)
+        try:
+            with open(reject_file_path, 'w') as reject_file:
+                trbk = traceback.format_exc()
+                reject_file_path = os.path.join(models_target_dir, 'modelling-rejected.txt')
+                reject_file.write(trbk)
+        except:
+            pass
 
     finally:
         os.chdir(current_dir)
