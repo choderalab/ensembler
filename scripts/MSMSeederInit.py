c85794cf67500962dbeefaedef79c53215d1b497
Daniel L. Parton <Danny.Parton@gmail.com>
2014-09-19 12:51:35 -0400

    Updated MSMSeederInit.py

--- scripts/MSMSeederInit.py
+++ scripts/MSMSeederInit.py
@@ -12,13 +12,11 @@ import msmseeder.initproject
 # Parse command-line arguments
 # ========
 
-def parse_args():
+def main():
     argparser = argparse.ArgumentParser(description='Initialize MSMSeeder project by creating necessary subdirectories and a project metadata .yaml file.')
     argparser.add_argument('--project_dir', type=str, default='.', help='(Default: ".") Optionally provide a directory path in which to initialize the project.')
     args = argparser.parse_args()
-    return args
 
-def main(args):
     project_dir = os.path.abspath(args.project_dir)
 
     if not os.path.exists(project_dir):
@@ -27,5 +25,4 @@ def main(args):
     msmseeder.initproject.init(project_dir)
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+    main()
