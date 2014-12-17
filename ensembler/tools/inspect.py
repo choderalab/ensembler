9fa4e56ef0736bd6cc54517bfd86dfb33ba8efc9
Daniel L. Parton <Danny.Parton@gmail.com>
2014-12-17 11:45:54 -0500

    Added tools package with inspect module

---
+++ ensembler/tools/inspect.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import ensembler
+import os
+import pandas as pd
+import yaml
+
+
+class LoopmodelLogs:
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+        self.df = self.parse_loopmodel_logs()
+
+    def parse_loopmodel_logs(self):
+        templateid = []
+        mpi_rank = []
+        hostname = []
+        successful = []
+        no_missing_residues = []
+        exception = []
+        loopmodel_exception = []
+        loopmodel_output = []
+        traceback = []
+        datestamp = []
+        timing_str = []
+        timing_total_seconds = []
+
+        structures_modeled_loops_dir = os.path.join(self.project_dir, os.path.join(ensembler.core.default_project_dirnames.templates_structures_modeled_loops))
+        for filename in os.listdir(structures_modeled_loops_dir):
+            if len(filename) >= 5:
+                if filename[-5:] == '.yaml':
+                    filepath = os.path.join(structures_modeled_loops_dir, filename)
+                    with open(filepath) as logfile:
+                        pass
+                        log = yaml.load(logfile, Loader=ensembler.core.YamlLoader)
+
+                        templateid.append(log.get('templateid'))
+                        mpi_rank.append(int(log.get('mpi_rank')))
+                        hostname.append(log.get('hostname'))
+                        successful.append(log.get('successful'))
+                        no_missing_residues.append(log.get('no_missing_residues'))
+                        exception.append(log.get('exception'))
+                        loopmodel_exception.append(log.get('loopmodel_exception'))
+                        loopmodel_output.append(log.get('loopmodel_output'))
+                        traceback.append(log.get('traceback'))
+                        ds = datetime.strptime(log.get('datestamp'), ensembler.core.datestamp_format_string)
+                        datestamp.append(ds)
+                        timing_str.append(log.get('timing'))
+                        timing_hours, timing_minutes, timing_seconds = map(int, timing_str[-1].split(':'))
+                        timing_total_seconds.append(timing_hours*3600 + timing_minutes*60 + timing_seconds)
+
+        df = pd.DataFrame(
+            {
+                'templateid': templateid,
+                'mpi_rank': mpi_rank,
+                'hostname': hostname,
+                'successful': successful,
+                'no_missing_residues': no_missing_residues,
+                'exception': exception,
+                'loopmodel_exception': loopmodel_exception,
+                'loopmodel_output': loopmodel_output,
+                'traceback': traceback,
+                'datestamp': datestamp,
+                'timing_str': timing_str,
+                'timing_total_seconds': timing_total_seconds,
+            }
+        )
+        return df
+
+    def to_hdf(self, ofilepath):
+        self.df.to_hdf(ofilepath, 'df')
+
+    def to_pickle(self, ofilepath):
+        self.df.to_pickle(ofilepath)
