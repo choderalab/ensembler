c9372a064769e410fd9f927bbfb9a14a50c12ebb
Daniel L. Parton <Danny.Parton@gmail.com>
2015-01-30 14:27:46 -0500

    Added rendering tool, and updated docs

--- docs/index.rst
+++ docs/index.rst
@@ -6,21 +6,43 @@
 Ensembler
 =====================================
 
-*Software pipeline for generating diverse protein configurational ensembles
-suitable for seeding highly parallel molecular simulations.*
+.. *Ensembler generates diverse protein configurational ensembles suitable for seeding highly parallel molecular simulations.*
 
 
-Overview of Pipeline
---------------------
+*Ensembler is a Python application and library which generates diverse protein
+configurational ensembles suitable for seeding highly parallel molecular
+simulations.*
 
-1. Gather protein target sequences and template structures.
-2. (*optional*) Reconstruct missing template loops, using ``Rosetta loopmodel``.
-3. Build models by mapping each target sequence onto each available template structure, using ``Modeller``.
-4. Culling of models based on close structural similarity.
-5. Refinement with implicit solvent molecular dynamics simulation, using ``OpenMM``.
-6. Solvate models.
-7. (*optional*) Refinement with explicit solvent molecular dynamics simulation, using ``OpenMM``.
-8. (*optional*) Package models, ready for transfer or set-up on other platforms such as ``Folding@Home``.
+Ensembler:
+ - Automates the time-consuming process of setting up protein systems for molecular simulation
+ - Exploits the entire variety of available genomic and structural data to provide diverse arrays of configurational models
+ - Allows the study of proteins across entire (super)families
+
+The generated models can be used as starting configurations for highly parallel
+molecular simulations, which take advantage of the increasing availability of
+parallel or distributed compute architectures. This approach is particularly
+beneficial when used in conjunction with recent techniques for constructing
+kinetic models, such as Markov state models, which can generate insight from
+multiple independent simulation trajectories.
+
+Ensembler can be run on a single computer or on a parallel compute cluster, and makes use of a number of external packages:
+ - `Modeller <https://salilab.org/modeller>`_ for comparative modeling of target sequences onto template structures
+ - `Rosetta <https://www.rosettacommons.org/software>`_ loopmodel for reconstruction of missing template loops
+ - `OpenMM <https://simtk.org/home/openmm>`_ for model refinement with highly efficient, GPU-acclerated, molecular dynamics simulation
+ - `MDTraj <http://mdtraj.org>`_ for ultra-fast RMSD calculation
+
+
+Overview of the Ensembler Pipeline
+----------------------------------
+
+1. Gather protein target sequences and template structures
+2. (*optional*) Reconstruct missing template loops, using ``Rosetta loopmodel``
+3. Build models by mapping each target sequence onto each available template structure, using ``Modeller``
+4. Culling of models based on close structural similarity
+5. Refinement with implicit solvent molecular dynamics simulation, using ``OpenMM``
+6. Solvate models
+7. (*optional*) Refinement with explicit solvent molecular dynamics simulation, using ``OpenMM``
+8. (*optional*) Package models, ready for transfer or set-up on other platforms such as ``Folding@Home``
 
 
 License
---
+++ ensembler/tools/rendering.py
@@ -0,0 +1,205 @@
+import subprocess
+import tempfile
+import os
+import warnings
+import shutil
+import ensembler
+import mdtraj
+import Image
+import ImageFont
+import ImageDraw
+
+
+class PymolRender(object):
+    def __init__(self, width=640, height=480):
+        self.width = width
+        self.height = height
+
+    def run_pymol(self, input_filepath=None, img_filepath=None):
+        input_filepath_abs = os.path.abspath(input_filepath)
+        img_filepath_abs = os.path.abspath(img_filepath)
+        pml_file_text = """\
+load {INPUT_FILEPATH}
+
+bg white
+set ray_trace_mode, 0
+set ambient, 1
+set reflect, 0
+set antialias, 1
+set two_sided_lighting, on
+set cartoon_fancy_helices, 1
+
+set_color dblue, [0.204, 0.298, 0.384]
+
+hide lines, all
+show cartoon, all
+color grey20, ss h
+color grey20, ss s
+color grey, ss l+''
+zoom all
+
+ray 640,480
+png {IMG_FILEPATH}
+"""
+        pml_file_text = pml_file_text.replace('{INPUT_FILEPATH}', input_filepath_abs)
+        pml_file_text = pml_file_text.replace('{IMG_FILEPATH}', img_filepath_abs)
+
+        tmpdir = tempfile.mkdtemp()
+
+        try:
+            pml_filepath = os.path.join(tmpdir, 'render.pml')
+            with open(pml_filepath, 'w') as pml_file:
+                pml_file.write(pml_file_text)
+
+            subprocess.call(['pymol', '-qc', pml_filepath])
+
+        finally:
+            shutil.rmtree(tmpdir)
+
+
+class Rendering(object):
+    def __init__(self):
+        self.targets, templates_resolved_seq, templates_full_seq = ensembler.core.get_targets_and_templates()
+        self.templates = templates_resolved_seq
+
+    def render_template_structure_resolved(self, templateid):
+        input_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_resolved, templateid+'.pdb')
+        img_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_resolved, templateid+'.png')
+        pr = PymolRender()
+        pr.run_pymol(input_filepath=input_filepath, img_filepath=img_filepath)
+
+    def render_template_structure_modeled_loops(self, templateid):
+        input_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_modeled_loops, templateid+'.pdb')
+        img_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_modeled_loops, templateid+'.png')
+        pr = PymolRender()
+        pr.run_pymol(input_filepath=input_filepath, img_filepath=img_filepath)
+
+    def render_model(self, targetid, templateid):
+        input_filepath = os.path.join(ensembler.core.default_project_dirnames.models, targetid, templateid, 'model.pdb.gz')
+        img_filepath = os.path.join(ensembler.core.default_project_dirnames.models, targetid, templateid, 'model.png')
+        pr = PymolRender()
+        pr.run_pymol(input_filepath=input_filepath, img_filepath=img_filepath)
+
+
+class RenderOnGrid(object):
+    def __init__(self, nrows=1, ncols=2, width=640, height=480):
+        self.structure_filepaths = []
+        self.labels = []
+        self.nrows = nrows
+        self.ncols = ncols
+        self.width = width
+        self.height = height
+        self._tmpdir = None
+
+    def add_template_structures_resolved(self, templateids, labels=None):
+        if type(templateids) is str:
+            templateids = [templateids]
+
+        if labels is None:
+            labels = [None] * len(templateids)
+
+        for i in range(len(templateids)):
+            templateid = templateids[i]
+            template_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_resolved, templateid+'.pdb')
+            self.structure_filepaths.append(template_filepath)
+            self.labels.append(labels[i])
+
+    def add_template_structures_modeled_loops(self, templateids, labels=None):
+        if type(templateids) is str:
+            templateids = [templateids]
+
+        if labels is None:
+            labels = [None] * len(templateids)
+
+        for i in range(len(templateids)):
+            templateid = templateids[i]
+            template_filepath = os.path.join(ensembler.core.default_project_dirnames.templates_structures_modeled_loops, templateid+'.pdb')
+            self.structure_filepaths.append(template_filepath)
+            self.labels.append(labels[i])
+
+    def add_model(self, targetid, templateid, label=None):
+        structure_filepath = os.path.join(ensembler.core.default_project_dirnames.models, targetid, templateid, 'model.pdb.gz')
+        self.structure_filepaths.append(structure_filepath)
+        self.labels.append(label)
+
+    def render(self, tiled_img_filename):
+        if len(self.structure_filepaths) == 0:
+            warnings.warn('No structures selected.')
+            return
+        self._tmpdir = tempfile.mkdtemp()
+        try:
+            self._align_structures()
+            self._render_aligned_structures()
+            self._tile_images(tiled_img_filename)
+        finally:
+            shutil.rmtree(self._tmpdir)
+
+    def _align_structures(self):
+        for irow in range(self.nrows):
+            row_first_col = irow * self.ncols
+            self._ref_traj = mdtraj.load_pdb(self.structure_filepaths[row_first_col])
+            self._heavy_atoms = self._ref_traj.topology.select('not name H')
+            for icol in range(self.ncols):
+                alignment_index = row_first_col + icol
+                structure_filepath = self.structure_filepaths[alignment_index]
+                self._align_structure(structure_filepath, alignment_index)
+
+    def _align_structure(self, structure_filepath, alignment_index):
+        traj = mdtraj.load_pdb(structure_filepath)
+        traj.superpose(self._ref_traj, atom_indices=self._heavy_atoms, parallel=False)
+        aligned_structure_filepath = os.path.join(self._tmpdir, 'aligned%d.pdb' % alignment_index)
+        traj.save(aligned_structure_filepath)
+
+    def _render_aligned_structures(self):
+        pr = PymolRender()
+        for structure_index in range(len(self.structure_filepaths)):
+            aligned_structure_filepath = os.path.join(self._tmpdir, 'aligned%d.pdb' % structure_index)
+            # if structure_index == 0:
+            #     shutil.copy(aligned_structure_filepath, '.')
+            img_filepath = os.path.join(self._tmpdir, 'img%d.png' % structure_index)   # TODO
+            pr.run_pymol(input_filepath=aligned_structure_filepath, img_filepath=img_filepath)
+
+    def _tile_images(self, tiled_img_filename, marl=0, marr=0, mart=0, marb=0, padding=0):
+        imgs = []
+        for structure_index in range(len(self.structure_filepaths)):
+            image_filename = os.path.join(self._tmpdir, 'img%d.png' % structure_index)
+            image = Image.open(image_filename)
+            # image = image.resize((self.width, self.height), Image.ANTIALIAS)
+            label = self.labels[structure_index]
+            if label is not None:
+                fontsize = 10
+                # font = ImageFont.truetype("HelveticaNeueLight.ttf", fontsize)
+                font = ImageFont.load_default()
+                draw = ImageDraw.Draw(image)
+                print label
+                draw.text((fontsize, 0), label, (0,0,0), font=font)
+            imgs.append(image)
+
+        # Calculate the size of the output image, based on the
+        #  photo thumb sizes, margins, and padding
+        marw = marl + marr
+        marh = mart + marb
+
+        padw = (self.ncols-1)*padding
+        padh = (self.nrows-1)*padding
+        isize = (self.ncols*self.width+marw+padw, self.nrows*self.height+marh+padh)
+
+        # Create the new image. The background doesn't have to be white
+        bgcolor = (255, 255, 255)
+        image = Image.new('RGB', isize, bgcolor)
+
+        # Insert each thumb:
+        for irow in range(self.nrows):
+            for icol in range(self.ncols):
+                left = marl + icol*(self.width+padding)
+                right = left + self.width
+                upper = mart + irow*(self.height+padding)
+                lower = upper + self.height
+                bbox = (left, upper, right, lower)
+                try:
+                    img = imgs.pop(0)
+                except:
+                    break
+                image.paste(img, bbox)
+
+        image.save(tiled_img_filename)
