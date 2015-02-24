9812b9bd39b99db1c5f7b8c3beaf4585e40a2435
Daniel L. Parton <Danny.Parton@gmail.com>
2015-02-24 14:49:30 -0500

    Added ModelingLogs tool

--- ensembler/tools/inspect.py
+++ ensembler/tools/inspect.py
@@ -160,94 +160,6 @@ class ProjectCounts(object):
         rates_df.to_csv(ofilepath, columns=self.output_columns)
 
 
-class LoopmodelLogs(object):
-    def __init__(self, project_dir='.'):
-        self.project_dir = project_dir
-        self.df = self.parse_loopmodel_logs()
-
-    def parse_loopmodel_logs(self):
-        templateid = []
-        mpi_rank = []
-        hostname = []
-        successful = []
-        no_missing_residues = []
-        exception = []
-        loopmodel_exception = []
-        loopmodel_output = []
-        traceback = []
-        datestamp = []
-        timing_str = []
-        timing_total_seconds = []
-
-        structures_modeled_loops_dir = os.path.join(self.project_dir, os.path.join(ensembler.core.default_project_dirnames.templates_structures_modeled_loops))
-        for filename in os.listdir(structures_modeled_loops_dir):
-            if len(filename) >= 5:
-                if filename[-5:] == '.yaml':
-                    filepath = os.path.join(structures_modeled_loops_dir, filename)
-                    with open(filepath) as logfile:
-                        pass
-                        log = yaml.load(logfile, Loader=ensembler.core.YamlLoader)
-
-                        templateid.append(log.get('templateid'))
-                        mpi_rank.append(int(log.get('mpi_rank')))
-                        hostname.append(log.get('hostname'))
-                        successful.append(log.get('successful'))
-                        no_missing_residues.append(log.get('no_missing_residues'))
-                        exception.append(log.get('exception'))
-                        loopmodel_exception.append(log.get('loopmodel_exception'))
-                        loopmodel_output.append(log.get('loopmodel_output'))
-                        traceback.append(log.get('traceback'))
-                        ds = datetime.strptime(log.get('datestamp'), ensembler.core.datestamp_format_string)
-                        datestamp.append(ds)
-                        timing_str.append(log.get('timing'))
-                        timing_hours, timing_minutes, timing_seconds = map(int, timing_str[-1].split(':'))
-                        timing_total_seconds.append(timing_hours*3600 + timing_minutes*60 + timing_seconds)
-
-        df = pd.DataFrame(
-            {
-                'templateid': templateid,
-                'mpi_rank': mpi_rank,
-                'hostname': hostname,
-                'successful': successful,
-                'no_missing_residues': no_missing_residues,
-                'exception': exception,
-                'loopmodel_exception': loopmodel_exception,
-                'loopmodel_output': loopmodel_output,
-                'traceback': traceback,
-                'datestamp': datestamp,
-                'timing_str': timing_str,
-                'timing_total_seconds': timing_total_seconds,
-            }
-        )
-        return df
-
-    def add_nmissing_resis_data(self):
-        nmissing_resis_df = []
-        for templateid in self.df.templateid:
-            loopfile_path = os.path.join(self.project_dir, 'templates/structures-modeled-loops', templateid + '.loop')
-            with open(loopfile_path) as loopfile:
-                lines = loopfile.readlines()
-                nmissing_resis = 0
-                for line in lines:
-                    loop_start = int(line[4:8])
-                    loop_end = int(line[8:12])
-                    nmissing_resis += loop_end - loop_start + 1
-            nmissing_resis_df.append({
-                'templateid': templateid,
-                'nmissing_resis': nmissing_resis,
-            })
-
-        nmissing_resis_df = pd.DataFrame(nmissing_resis_df)
-
-        self.df = self.df.merge(nmissing_resis_df, on='templateid')
-
-    def to_hdf(self, ofilepath):
-        self.df.to_hdf(ofilepath, 'df')
-
-    def to_pickle(self, ofilepath):
-        self.df.to_pickle(ofilepath)
-
-
 class ModelSimilarities(object):
     def __init__(self, targetid, project_dir='.', log_level=None):
         ensembler.utils.loglevel_setter(logger, log_level)
@@ -346,4 +258,46 @@ class ModelSimilarities(object):
     def to_csv(self, ofilepath=None):
         if ofilepath is None:
             ofilepath = os.path.join(self.model_dir, 'rmsds.csv')
+        self.df.to_csv(ofilepath)
+
+
+class ModelingLogs(object):
+    def __init__(self, targetid, project_dir='.'):
+        self.project_dir = project_dir
+        self.df = self.parse_logs()
+        self.targetid = targetid
+
+    def parse_logs(self):
+        self.target_models_dir = os.path.join(self.project_dir, ensembler.core.default_project_dirnames.models, self.targetid)
+        log_data = {}
+        root, dirs, files = next(os.walk(self.target_models_dir))
+        for dirname in dirs:
+            if re.match(ensembler.core.template_id_regex, dirname):
+                logfilepath = os.path.join(self.target_models_dir, dirname, 'modeling-log.yaml')
+                if os.path.exists(logfilepath):
+                    with open(logfilepath) as logfile:
+                        log = yaml.load(logfile)
+
+                    for key in log:
+                        if key not in log_data:
+                            log_data[key] = [log[key]]
+                        else:
+                            log_data[key].append(log[key])
+
+                        if key == 'timing':
+                            if re.match('[0-9]+:[0-9]+:[0-9]+', log[key]):
+                                hours, mins, seconds = [int(x) for x in log[key].split(':')]
+                                timing_total_seconds = datetime.timedelta(seconds=(seconds + mins*60 + hours*3600))
+                            else:
+                                timing_total_seconds = None
+
+                            if 'timing_total_seconds' not in log_data:
+                                log_data['timing_total_seconds'] = [timing_total_seconds]
+                            log_data['timing_total_seconds'].append(timing_total_seconds)
+
+        df = log_data
+
+        return df
+
+    def to_csv(self, ofilepath):
         self.df.to_csv(ofilepath)
