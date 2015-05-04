6ae1f50b1d070d85773d570cdf34318361b46e78
Daniel L. Parton <Danny.Parton@gmail.com>
2015-05-04 19:16:43 -0400

    Added Travis CI scripts

---
+++ devtools/ci/install.sh
@@ -0,0 +1,17 @@
+MINICONDA=Miniconda-latest-Linux-x86_64.sh
+MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget http://repo.continuum.io/miniconda/$MINICONDA
+if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+    echo "Miniconda MD5 mismatch"
+    exit 1
+fi
+bash $MINICONDA -b
+PIP_ARGS="-U"
+
+export PATH=$HOME/miniconda/bin:$PATH
+
+sudo apt-get update
+
+conda update --yes conda
+conda config --add channels http://conda.binstar.org/omnia
+source activate $python
