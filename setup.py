1ff678d4095d1185c3e184b296f2614f6dc6990a
Daniel L. Parton <Danny.Parton@gmail.com>
2015-06-18 15:19:17 -0400

    Making 1.0 release - updated version info

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import subprocess
 from setuptools import setup
 
 ##########################
-VERSION = "0.2"
-ISRELEASED = False
+VERSION = "1.0"
+ISRELEASED = True
 __version__ = VERSION
 ##########################
 
