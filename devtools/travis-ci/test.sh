b888e0f0bf1a14dab5604c0cbb4c6a435e5bed1d
Daniel L. Parton <Danny.Parton@gmail.com>
2015-07-23 22:51:13 -0400

    Trying to get Travis working in original repo

--- devtools/travis-ci/test.sh
+++ devtools/travis-ci/test.sh
@@ -6,7 +6,9 @@ then
     conda build devtools/conda-recipe
     conda install --yes --use-local ensembler
     conda install --yes nose
+    cd /
     nosetests ensembler -v --exe -a modeller
+    cd
 else
     echo "This is a pull request. Secure environment variables are not available, so will not attempt to run Modeller tests."
 fi
