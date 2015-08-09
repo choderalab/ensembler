a8851550cee345ef81fc86e882f657d83948c5a2
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-08-09 15:06:50 -0700

    Updates to get conda build to work.

--- devtools/travis-ci/test.sh
+++ devtools/travis-ci/test.sh
@@ -4,9 +4,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
 then
     conda install --yes --use-local ensembler
     conda install --yes nose
-    cd /
     nosetests ensembler -v --exe -a modeller
-    cd
 else
     echo "This is a pull request. Secure environment variables are not available, so will not attempt to run Modeller tests."
 fi
