08eba4368b1eb1aa6bfc859834fffc30d4f35756
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-08-09 16:29:02 -0700

    Updated test script to make nosetests work.

--- devtools/travis-ci/test.sh
+++ devtools/travis-ci/test.sh
@@ -4,7 +4,9 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
 then
     conda install --yes --use-local ensembler
     conda install --yes nose
+    pushd .; cd /
     nosetests ensembler -v --exe -a modeller
+    popd
 else
     echo "This is a pull request. Secure environment variables are not available, so will not attempt to run Modeller tests."
 fi
