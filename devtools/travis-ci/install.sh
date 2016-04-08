cd587f4441e015bda48c5ba620c9e1083e01988b
jchodera <john.chodera@choderalab.org>
2016-04-08 17:49:15 -0400

    Install miniconda into $HOME/miniconda

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -6,7 +6,7 @@ if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b
+bash $MINICONDA -b -p $HOME/miniconda
 
 export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 anaconda-client pip
