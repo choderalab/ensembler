f99bcad6efa98ee47934239377a85f7bb44fe272
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-12-30 18:38:14 -0500

    more tinkering with dependencies

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -3,7 +3,7 @@ pushd .
 cd $HOME
 
 # Install Miniconda
-MINICONDA=Miniconda2-latest-Linux-x86_64.sh
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_HOME=$HOME/miniconda
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget -q https://repo.continuum.io/miniconda/$MINICONDA
